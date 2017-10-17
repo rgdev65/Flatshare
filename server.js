f3bc80c4ede0cbfe2abcf5001a1f1dcdecc8b70f
Rahul <apkay23@gmail.com>
2017-10-17 17:11:41 +0530

    added change suggested by Adam

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post('/landing',urlencodedParser,function(req,res){
     var oldData;
     var newData
     var dataObj= req.body;
-    var newTodo = Todo(dataObj).save((err,data)=>{
+    var newTodo = (new Todo(dataObj)).save((err,data)=>{
       if (err) {
         throw err;
       }
