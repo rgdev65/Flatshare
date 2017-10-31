fb8fec59f3574c032301d22f982631c1969f736e
Priyanka Negi <prinegi100@gmail.com>
2017-11-01 01:12:26 +0530

    set heroko db path

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //database connection
-mongoose.connect('mongodb://localhost/flateShare');
+mongoose.connect('mongodb://heroku_1chmdmmw:invg6cbj921n87hp14mr9eadof@ds243335.mlab.com:43335/heroku_1chmdmmw');
 
 
 module.exports = mongoose;
