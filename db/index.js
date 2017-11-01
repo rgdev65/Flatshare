93ebfb8169d10362eeaccc4ba5a05c996ca1aeca
Priyanka Negi <prinegi100@gmail.com>
2017-11-01 10:20:23 +0530

    added config

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
+ require('../config');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
 //database connection
-mongoose.connect('mongodb://heroku_1chmdmmw:invg6cbj921n87hp14mr9eadof@ds243335.mlab.com:43335/heroku_1chmdmmw');
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports = mongoose;
