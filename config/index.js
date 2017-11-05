3cae7c1eb19fbbb913eb4d04b1dfb4e89577de50
Ashok Dey <ashokdey100@gmail.com>
2017-11-05 17:07:46 +0530

    --fix : options for mongodb connection

--- config/index.js
+++ config/index.js
@@ -3,5 +3,4 @@ const environment = process.env.NODE_ENV || 'develop';
 if(environment === 'develop'){
   process.env.PORT = 5000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/flatShare';
-
 }
--- db/index.js
+++ db/index.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-console.log(process.env.MONGODB_URI);
-
 //database connection
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 
 module.exports = mongoose;
