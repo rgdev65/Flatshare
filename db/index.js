90931193656d1e95d9d5a008286b964388f75058
Ashok Dey <ashokdey100@gmail.com>
2017-11-05 17:06:00 +0530

    --fix : update in process variable name

--- db/index.js
+++ db/index.js
@@ -1,7 +1,9 @@
- require('../config');
+require('../config');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
+console.log(process.env.MONGODB_URI);
 
 //database connection
 mongoose.connect(process.env.MONGODB_URI);
